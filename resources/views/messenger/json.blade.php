466ae7ba305ac7fe71b8136322fb15f88c9e2e0f
Anthony Delgado <me@anthonydelgado.me>
2016-05-17 17:49:32 -0400

    Update json.blade.php

--- resources/views/messenger/json.blade.php
+++ resources/views/messenger/json.blade.php
@@ -9,20 +9,7 @@ fitlerkey: '',
 order: 1,
 
 messages: [
-@if($threads->count() > 0)
-    @foreach($threads as $thread)
-        {
-        "id": "{{ $thread->id }}",
-        "name": "{{ $thread->creator()->name }}",
-        "date": "{{ $thread->id }}",
-        "body": "{{ $thread->latestMessage->body }}",
-        "avatar": "{{  Gravatar::get(Auth::user()->email) }}",
-        "url": "{{URL::to('messages/' . $thread->id)}}",
-        },
-    @endforeach
-@else
-    {id: '0', name: '', date: '', body: 'No messages yet.', avatar: '{{  Gravatar::get(Auth::user()->email) }}', url: '{{URL::to('messages')}}'},
-@endif
+ 
 ]
 },
 
@@ -31,5 +18,19 @@ sortvia: function (sortparam, order) {
 this.order = this.order * -1;
 this.sortparam = sortparam;
 }
-}
+}, 
+ready: function() {
+
+var self = this; 
+
+      // GET request
+      this.$http({url: '/messages/json', method: 'GET'}).then(function (response) {
+          // success callback
+          self.messages = response.data; 
+      }, function (response) {
+          // error callback
+      });
+
+    }
 });
+
